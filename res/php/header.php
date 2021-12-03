f7aa518d64a73bb70d5a877ea64eac7349bd111e
Skylar <Skylar.beacham@outlook.com>
2021-12-03 23:37:13 +0000

    Update gitignore and header file

--- res/php/header.php
+++ res/php/header.php
@@ -6,14 +6,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="apple-touch-icon" sizes="180x180" href="res/favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="res/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="res/favicon/favicon-16x16.png">
-    <link rel="manifest" href="res/favicon/site.webmanifest">
-    <link rel="mask-icon" href="res/favicon/safari-pinned-tab.svg" color="#0b2033">
-    <link rel="shortcut icon" href="res/favicon/favicon.ico">
+    <link rel="apple-touch-icon" sizes="180x180" href="../favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../favicon/favicon-16x16.png">
+    <link rel="manifest" href="../favicon/site.webmanifest">
+    <link rel="mask-icon" href="../favicon/safari-pinned-tab.svg" color="#0b2033">
+    <link rel="shortcut icon" href="../favicon/favicon.ico">
     <meta name="msapplication-TileColor" content="#0b2033">
-    <meta name="msapplication-config" content="res/favicon/browserconfig.xml">
+    <meta name="msapplication-config" content="../favicon/browserconfig.xml">
     <meta name="theme-color" content="#0b2033">
     <title><?php echo($title); ?></title>
     <!-- End Metadata -->
