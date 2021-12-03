0813a77cc8699e8f1b475a85f6b17b4dcea70795
Skylar <skylar@vixendev.com>
2021-12-03 12:13:35 +0000

    Add base resources

---
+++ res/php/functions.inc.php
@@ -0,0 +1,34 @@
+<?php
+
+    function GenerateID() {
+        $IDData = $IDData ?? random_bytes(16);
+        assert(strlen($IDData) == 16);
+        $IDData[6] = chr(ord($IDData[6]) & 0x0f | 0x40);
+        $IDData[8] = chr(ord($IDData[8]) & 0x3f | 0x80);
+        return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($IDData), 4));
+    }
+
+    function getGreeting(){
+        $hour = date("H");
+        if($hour >= 0 && $hour < 12){
+            return "Good Morning";
+        }else if($hour >= 12 && $hour < 18){
+            return "Good Afternoon";
+        }else if($hour >= 18 && $hour < 24){
+            return "Good Evening";
+        }
+    }
+
+    function getUser(){
+        include("_connect.php");
+        $sql = "SELECT * FROM `tblUsers` WHERE `tblUsers`.`UUID` = ?";
+        $stmt = mysqli_prepare($connect, $sql);
+        mysqli_stmt_bind_param($stmt, 's', $_SESSION["userID"]);
+        $stmt -> execute();
+        $result = $stmt->get_result();
+        if($result -> num_rows === 1){
+            $User = $result->fetch_array(MYSQLI_ASSOC);
+            return $User;
+        }
+    }
+?>
