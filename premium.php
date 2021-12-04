9567cc6ea292ad72017d5e13501806c81baf3037
Skylar <Skylar.beacham@outlook.com>
2021-12-04 13:26:09 +0000

    Add borders and center columns

--- premium.php
+++ premium.php
@@ -19,7 +19,7 @@
     <div class="row mt-5">
         <div class="row">
             <div class="col-12 col-md-6 col-lg-6 mb-1">
-                <div class="card">
+                <div class="card text-center border-dark">
                     <div class="card-body">
                         <h5 class="card-title">Free</h5>
                         <p class="card-text"><ul>
@@ -34,7 +34,7 @@
                 </div>
             </div>
             <div class="col-12 col-md-6 col-lg-6 mb-1">
-                <div class="card">
+                <div class="card text-center border-dark">
                     <div class="card-body">
                         <h5 class="card-title">Premium</h5>
                         <p class="card-text"><ul>
