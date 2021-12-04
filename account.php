df10c2a4a26483a986451888a1eb2f3b168c3945
Skylar <Skylar.beacham@outlook.com>
2021-12-04 00:11:58 +0000

    Added navbar to main pages

--- account.php
+++ account.php
@@ -1,5 +1,5 @@
-<?php $title="Home | VDBP"; ?>
-<?php $currentPage="text"; ?>
+<?php $title="Account | VDBP"; ?>
+<?php $currentPage="account"; ?>
 <?php include("res/php/header.php"); ?>
 <?php include("res/php/navbar.php"); ?>
 <?php include("res/php/functions.inc.php"); ?>
@@ -11,7 +11,7 @@
     <!-- Welcome Greeting -->
     <div class="row">
         <div class="col-12 mt-5 align-items-center">
-            <h1 class="text-center">Text</h1>
+            <h1 class="text-center">Account</h1>
         </div>
     </div>
     <!-- End Welcome Greeting -->
---
+++ newbaste.php
@@ -0,0 +1,25 @@
+<?php $title="New Baste | VDBP"; ?>
+<?php $currentPage="newbaste"; ?>
+<?php include("res/php/header.php"); ?>
+<?php include("res/php/navbar.php"); ?>
+<?php include("res/php/functions.inc.php"); ?>
+
+
+<!-- Main Page Content -->
+<div class="container">
+
+    <!-- Welcome Greeting -->
+    <div class="row">
+        <div class="col-12 mt-5 align-items-center">
+            <h1 class="text-center">New Baste</h1>
+        </div>
+    </div>
+    <!-- End Welcome Greeting -->
+
+    <br>
+
+</div>
+<!-- End Main Page Content -->
+
+
+<?php include("./res/php/footer.php"); ?>
