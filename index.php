fd913d174fc2886c8e9cfc3589806aff17556fd1
Skylar <Skylar.beacham@outlook.com>
2021-12-04 13:08:15 +0000

    Modify SQL and output

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <?php include("res/php/navbar.php"); ?>
 <?php include("res/php/functions.inc.php"); ?>
 <?php $User = GetUser($connect); ?>
-<?php $sql = "SELECT * FROM `tblBastes` WHERE `tblBastes`.`Visibility` = ? ORDER BY `tblBastes`.`CreatedAt` DESC LIMIT 5"; ?>
+<?php $sql = "SELECT * FROM `tblBastes` WHERE `tblBastes`.`Visibility` = 2 ORDER BY `tblBastes`.`CreatedAt` DESC LIMIT 5"; ?>
 <?php $query = mysqli_query($connect, $sql); ?>
 
 
@@ -15,7 +15,7 @@
     <!-- Welcome Greeting -->
     <div class="row">
         <div class="col-12 mt-5 align-items-center">
-            <h1 class="text-center"><?php echo getGreeting(); ?>, <?php echo $User["FirstName"] . " ". $User["LastName"];?></h1>
+            <h1 class="text-center"><?php echo getGreeting(); ?>, <?php echo $User["FirstName"];?> <?php echo $User["LastName"];?></h1>
         </div>
     </div>
     <!-- End Welcome Greeting -->
@@ -29,7 +29,7 @@
                         <div class="card-body">
                             <h5 class="card-title"><?php echo $rows['BasteName']; ?></h5>
                             <p class="card-text"><?php echo $rows['CreatedAt']; ?></p>
-                            <a href="baste.php?id=<?php echo $rows['BasteID']; ?>" class="btn btn-primary">View Baste</a>
+                            <a href="baste/<?php echo $rows['BasteID']; ?>" class="btn btn-primary">View Baste</a>
                         </div>
                     </div>
                 </div>
