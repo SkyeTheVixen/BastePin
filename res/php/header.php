5de50c03e904346e56d1cefcb325bca6b570126b
Skylar <Skylar.beacham@outlook.com>
2021-12-06 20:29:58 +0000

    Modify header to account for weird file path locations

--- res/php/header.php
+++ res/php/header.php
@@ -1,3 +1,7 @@
+<?php if($currentPage == "baste" || $currentPage == "account"){$pathHead = "../res/";} else {$pathHead = "res/";}?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,20 +10,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="apple-touch-icon" sizes="180x180" href="res/favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="res/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="res/favicon/favicon-16x16.png">
-    <link rel="manifest" href="res/favicon/site.webmanifest">
-    <link rel="mask-icon" href="res/favicon/safari-pinned-tab.svg" color="#0b2033">
-    <link rel="shortcut icon" href="res/favicon/favicon.ico">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo $pathHead;?>favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo $pathHead;?>favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo $pathHead;?>favicon/favicon-16x16.png">
+    <link rel="manifest" href="<?php echo $pathHead;?>favicon/site.webmanifest">
+    <link rel="mask-icon" href="<?php echo $pathHead;?>favicon/safari-pinned-tab.svg" color="#0b2033">
+    <link rel="shortcut icon" href="<?php echo $pathHead;?>favicon/favicon.ico">
     <meta name="msapplication-TileColor" content="#0b2033">
-    <meta name="msapplication-config" content="res/favicon/browserconfig.xml">
+    <meta name="msapplication-config" content="<?php echo $pathHead;?>favicon/browserconfig.xml">
     <meta name="theme-color" content="#0b2033">
     <title><?php echo($title); ?></title>
     <!-- End Metadata -->
 
     <!-- Stylesheets -->
-    <link rel="stylesheet" href="res/css/<?php echo($currentPage); ?>.css">
+    <link rel="stylesheet" href="<?php echo $pathHead;?>css/<?php echo($currentPage); ?>.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.3.1/build/styles/default.min.css">
     <!-- End Stylesheets -->
@@ -29,7 +33,7 @@
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.11.3/js/dataTables.bootstrap5.min.js"></script>
-    <script type="text/javascript" src='res/js/<?php echo($currentPage); ?>.js'></script>
+    <script type="text/javascript" src='<?php echo $pathHead;?>js/<?php echo($currentPage); ?>.js'></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/93e867abff.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.3.1/build/highlight.min.js"></script>
