8dc427f9ae0b0348c0c9079fc05a0f9d732a1ad0
Skylar <skylar@vixendev.com>
2021-12-09 11:53:17 +0000

    Whoops

--- res/php/_authcheck.php
+++ res/php/_authcheck.php
@@ -1,9 +1,9 @@
 <?php
     session_start();
     if (!isset($_SESSION['UserID']) && ($currentPage === "account" || $currentPage === "baste")) {
-        header("Location: ../../login");
+        header("Location: ../login");
     }
     else if (!isset($_SESSION['UserID'])){
-        header("Location: ../login");
+        header("Location: login");
     }
 ?>
