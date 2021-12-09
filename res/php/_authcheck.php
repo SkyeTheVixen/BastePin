7205692d29a9ee5a05f9f402052eb31da3ef71d9
Skylar <skylar@vixendev.com>
2021-12-09 11:44:27 +0000

    update authcheck script

--- res/php/_authcheck.php
+++ res/php/_authcheck.php
@@ -1,6 +1,9 @@
 <?php
     session_start();
-    if (!isset($_SESSION['UserID'])){
+    if (!isset($_SESSION['UserID']) && ($currentPage === "account" || $currentPage === "baste")) {
+        header("Location: ../../login");
+    }
+    else if (!isset($_SESSION['UserID'])){
         header("Location: ../login");
     }
 ?>
