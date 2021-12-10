7243b593fa2e716023045700f26681e00d7a49a6
Skylar <skylar@vixendev.com>
2021-12-10 11:06:37 +0000

    not

--- baste.php
+++ baste.php
@@ -11,7 +11,7 @@
 
 <!-- Main Page Content -->
 <div class="container">
-    <?php if($_GET["BasteID"] =! "") {?>
+    <?php if(!($_GET["BasteID"] == "")) {?>
         <!-- Welcome Greeting -->
         <div class="row">
             <div class="col-12 mt-5 align-items-center">
