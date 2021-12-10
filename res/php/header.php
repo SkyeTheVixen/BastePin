6e4ae78b02d1ac3b864ee3f84f7e0a7fbefd2f84
Skylar <skylar@vixendev.com>
2021-12-10 11:23:41 +0000

    header update

--- res/php/header.php
+++ res/php/header.php
@@ -26,6 +26,7 @@
     <link rel="stylesheet" href="<?php echo $pathHead;?>css/<?php echo($currentPage); ?>.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.3.1/build/styles/default.min.css">
+    <?php if($currentPage == "baste"){?><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.3.1/styles/default.min.css"><?php }?>
     <!-- End Stylesheets -->
 
     <!-- Important Scripts -->
@@ -37,4 +38,5 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/93e867abff.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.3.1/build/highlight.min.js"></script>
+    <?php if($currentPage == "baste"){?><script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.3.1/highlight.min.js"></script><?php }?>
 </head>
