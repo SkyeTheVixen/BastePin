be99af03e69189f171f70a9b890f20c6b0c16b90
Skylar <Skylar.beacham@outlook.com>
2021-12-17 12:32:27 +0000

    Cant header?

--- baste.php
+++ baste.php
@@ -9,7 +9,7 @@
     if(isset($_GET["BasteID"])) {
         $baste = getBaste($connect, $_GET["BasteID"]);
         if($baste["ExpiresAt"] < date("Y-m-d H:i:s")){
-            header("Location: expired");
+            echo "<script>window.location.href=\"expired\"</script>";
         }
     }
 ?>
