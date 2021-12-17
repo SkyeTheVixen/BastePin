bcbb629288ac91a059c31ccec5b75dbaae660c80
Skylar <Skylar.beacham@outlook.com>
2021-12-17 12:50:10 +0000

    Positioning and size for mobile

--- index.php
+++ index.php
@@ -44,10 +44,10 @@
             <?php } ?>
         </div>
         <div class="row mt-5">
-            <div class="col-8">
+            <div class="col-6 col-sm-12">
             <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/5GJWxDKyk3A?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
-            <div class="col-4">
+            <div class="col-6 col-sm-12">
                 <!-- Sidebar -->
             </div>
         </div>
