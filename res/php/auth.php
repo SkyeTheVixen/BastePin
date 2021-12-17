0b8670d669f99bd1cc1d32a971b3ed5230fce8f6
Skylar <Skylar.beacham@outlook.com>
2021-12-17 10:37:15 +0000

    Hmmm... Maybe thisll actualy function

--- res/php/auth.php
+++ res/php/auth.php
@@ -16,69 +16,7 @@
     $result = $stmt->get_result();
     $mysqli -> commit();
 
-    //If the ID doesnt exist in the db, then proceed as normal
-    if($result -> num_rows === 0){
-        $stmt -> close();
-        //Check the inputs are filled out
-        if(isset($_POST["txtUser"]) && isset($_POST["txtPassword"])){
-            //Get data POSTED
-            $email = mysqli_real_escape_string($connect, $_POST["txtUser"]);
-            $password = mysqli_real_escape_string($connect, $_POST["txtPassword"]);
-            
-            //Check email matches format
-            if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-                echo json_encode(array("statusCode" => 202));
-            }
-            
-            //SQL Query
-            $mysqli -> autocommit(false);
-            $sql = "SELECT * FROM `tblUsers` WHERE `tblUsers`.`Email` = ?";
-            $stmt = $mysqli ->prepare($sql);
-            $stmt -> bind_param('s', $email);
-            $stmt -> execute();
-            $result = $stmt->get_result();
-            $mysqli->commit();
-            
-            //If the email exists in the db, then proceed as normal
-            if($result -> num_rows === 1){
-                $User = $result->fetch_array(MYSQLI_ASSOC);
-                $stmt -> close();
-                //If password matches
-                if(password_verify($password, $User["Password"], ))
-                {
-                    //Check if user account is locked
-                    if($User["IsLocked"] == 1){
-                        //Return locked
-                        echo json_encode(array("statusCode" => 204));
-                    }
-                    else{
-                        //Return success
-                        $_SESSION["UserID"] = $User["UserID"];
-                        echo json_encode(array("UserID" => $User["UserID"], "statusCode" => 200));
-                    }
-                }
-                else{
-                    //Return invalid credentials
-                    $mysqli -> autocommit(false);
-                    $sql = "UPDATE `tblBFA` SET `Tries` = `Tries` + 1 WHERE `ID` = ?";
-                    $stmt = $mysqli -> prepare($sql);
-                    $stmt -> bind_param('s', $id);
-                    $stmt -> execute();
-                    $mysqli -> commit();
-                    $stmt -> close();
-                    echo json_encode(array("statusCode" => 201));
-                }
-            }
-            else{
-                //Return invalid credentials
-                echo json_encode(array("statusCode" => 202));
-            }
-        }
-        else{
-            echo json_encode(array("statusCode" => 203));
-        }
-    }
-    else if($result -> num_rows === 1){
+    if($result -> num_rows === 1){
         $stmt->close();
         //If user IP has been blocked
         if ($result["Blocked"] == 1) {
@@ -96,6 +34,66 @@
             $stmt -> close();
         }
     }
+
+    //Check the inputs are filled out
+    if(isset($_POST["txtUser"]) && isset($_POST["txtPassword"])){
+        //Get data POSTED
+        $email = mysqli_real_escape_string($connect, $_POST["txtUser"]);
+        $password = mysqli_real_escape_string($connect, $_POST["txtPassword"]);
+        
+        //Check email matches format
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+            echo json_encode(array("statusCode" => 202));
+        }
+        
+        //SQL Query
+        $mysqli -> autocommit(false);
+        $sql = "SELECT * FROM `tblUsers` WHERE `tblUsers`.`Email` = ?";
+        $stmt = $mysqli ->prepare($sql);
+        $stmt -> bind_param('s', $email);
+        $stmt -> execute();
+        $result = $stmt->get_result();
+        $mysqli->commit();
+        
+        //If the email exists in the db, then proceed as normal
+        if($result -> num_rows === 1){
+            $User = $result->fetch_array(MYSQLI_ASSOC);
+            $stmt -> close();
+            //If password matches
+            if(password_verify($password, $User["Password"], ))
+            {
+                //Check if user account is locked
+                if($User["IsLocked"] == 1){
+                    //Return locked
+                    echo json_encode(array("statusCode" => 204));
+                }
+                else{
+                    //Return success
+                    $_SESSION["UserID"] = $User["UserID"];
+                    echo json_encode(array("UserID" => $User["UserID"], "statusCode" => 200));
+                }
+            }
+            else{
+                //Return invalid credentials
+                $mysqli -> autocommit(false);
+                $sql = "INSERT INTO `tblBFA` (`ID`, `Tries`) VALUES(?, 1) ON DUPLICATE KEY UPDATE `Tries` = `Tries` + 1";
+                $stmt = $mysqli -> prepare($sql);
+                $stmt -> bind_param('s', $id);
+                $stmt -> execute();
+                $mysqli -> commit();
+                $stmt -> close();
+                echo json_encode(array("statusCode" => 201));
+            }
+        }
+        else{
+            //Return invalid credentials
+            echo json_encode(array("statusCode" => 202));
+        }
+    }
+    else{
+        echo json_encode(array("statusCode" => 203));
+    }
+
 
     $mysqli -> close();
 
