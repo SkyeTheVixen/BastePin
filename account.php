e075a552a7f9e2a0294cde40cdfc4cf034700c7c
Skylar <Skylar.beacham@outlook.com>
2021-12-22 21:32:23 +0000

    add account details and a dump for temp

--- account.php
+++ account.php
@@ -24,12 +24,22 @@
         <div class="col-12">
             <div class="row">
                 <div class="col-8">
-                    <h1><?php echo $user["FirstName"]." ".$user["LastName"]; ?></h1>
+                    <h1><?php echo htmlspecialchars($user["FirstName"]." ".$user["LastName"]); ?></h1>
                 </div>
                 <div class="col-4">
                     <img src="https://proficon.stablenetwork.uk/api/identicon/<?php echo $user["UserID"];?>.svg" alt="Profile Icon" class="h-25"/>
                 </div>
-
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <h5>Name: <?php echo htmlspecialchars($user["FirstName"]." ".$user["LastName"]);?></h5>
+                <h5>Email: <?php echo htmlspecialchars($user["Email"]);?></h5>
+                <h5>Can Baste?: <?php echo htmlspecialchars($user["CanBaste"]);?></h5>
+                <h5>IsAdmin?: <?php echo htmlspecialchars($user["IsAdmin"]);?></h5>
+                <h5>IsPremium?: <?php echo htmlspecialchars($user["IsPremium"]);?></h5>
+                <h5>IsLocked?: <?php echo htmlspecialchars($user["IsLocked"]);?></h5>
+                <h5>Baste Count: <?php echo htmlspecialchars($user["BasteCount"]);?>/<?php echo htmlspecialchars($user["MaximumBastes"]);?></h5>
+                <?php var_dump($user);?>
         </div>
     </div>
 </div>
