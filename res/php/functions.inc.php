b5420dd0a0e6e0021e6afe3bdf34e044fa763612
Skylar <Skylar.beacham@outlook.com>
2021-12-22 23:00:49 +0000

    And some more needed

--- res/php/functions.inc.php
+++ res/php/functions.inc.php
@@ -4,8 +4,12 @@
     use PHPMailer\PHPMailer\Exception;
     if(file_exists("../vendor/autoload.php")){
         require '../vendor/autoload.php';
-    }else{
+    }else if(file_exists("../../vendor/autoload.php")){
         require '../../vendor/autoload.php';
+    }else if(file_exists("vendor/autoload.php")){
+        require 'vendor/autoload.php';
+    }else if(file_exists("./vendor/autoload.php")){
+        require './vendor/autoload.php';
     }
 
     function GenerateID() {
