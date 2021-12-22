3b09b7e40f46bcf4281b0d15dcc1c05b04b996d6
Skylar <Skylar.beacham@outlook.com>
2021-12-22 20:57:38 +0000

    User details first population?

--- account.php
+++ account.php
@@ -23,13 +23,13 @@
     <div class="row">
         <div class="col-12">
             <div class="row">
+                <div class="col-8">
+                    <h1><?php echo $user["FirstName"]." ".$user["LastName"]; ?></h1>
+                </div>
                 <div class="col-4">
                     <img src="https://proficon.stablenetwork.uk/api/identicon/<?php echo $user["UserID"];?>.svg" alt="Profile Icon" />
                 </div>
-                <div class="col-4">
-                </div>
-                <div class="col-4">
-                </div>
+
         </div>
     </div>
 </div>
