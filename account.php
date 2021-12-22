887aff1a831bd857ae330efbf0c41eded01cf10e
Skylar <Skylar.beacham@outlook.com>
2021-12-22 20:56:20 +0000

    Profile Image

--- account.php
+++ account.php
@@ -1,10 +1,12 @@
 <?php session_start();?>
 <?php $title="Account | VDBP"; ?>
 <?php $currentPage="account"; ?>
+<?php include("res/php/_connect.php"); ?>
 <?php include("res/php/_authcheck.php"); ?>
 <?php include("res/php/header.php"); ?>
 <?php include("res/php/navbar.php"); ?>
 <?php include("res/php/functions.inc.php"); ?>
+<?php $user = getUser($connect); ?>
 
 
 <!-- Main Page Content -->
@@ -18,8 +20,18 @@
     </div>
     <!-- End Welcome Greeting -->
 
-    <br>
-
+    <div class="row">
+        <div class="col-12">
+            <div class="row">
+                <div class="col-4">
+                    <img src="https://proficon.stablenetwork.uk/api/identicon/<?php echo $user["UserID"];?>.svg" alt="Profile Icon" />
+                </div>
+                <div class="col-4">
+                </div>
+                <div class="col-4">
+                </div>
+        </div>
+    </div>
 </div>
 <!-- End Main Page Content -->
 
