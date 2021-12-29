988737def3ee98af44e0a7bb0d43011666c53002
Skylar <Skylar.beacham@outlook.com>
2021-12-29 13:34:11 +0000

    added user bastes to profile

--- profile.php
+++ profile.php
@@ -6,7 +6,8 @@
 <?php include("res/php/header.php"); ?>
 <?php include("res/php/navbar.php"); ?>
 <?php include("res/php/functions.inc.php"); ?>
-<?php $user = getProfileById($connect, $_GET["UserID"]); ?>
+<?php $userID = mysqli_real_escape_string($connect, $_GET["UserID"])?>
+<?php $user = getProfileById($connect, $userID); ?>
 <!-- If there is an error -->
 <?php
     if(isset($_GET["er"])) {
@@ -16,6 +17,17 @@
             echo "<script>Swal.fire({ icon: 'warning', title: 'Oops...', text: 'There was no baste to delete', heightAuto: false });</script>"; 
         }
     } 
+?>
+
+<?php
+    $mysqli -> autocommit(FALSE);
+    $sql = "SELECT * FROM `tblBastes` WHERE  `tblBastes`.`Visibility` = 2 AND `tblBastes`.`UserID` = ? ORDER BY `tblBastes`.`CreatedAt` DESC";
+    $stmt = $mysqli -> prepare($sql);
+    $stmt -> bind_param('s', $userID);
+    $stmt -> execute();
+    $result = $stmt->get_result();
+    $mysqli -> commit();
+    $stmt -> close();
 ?>
 
 <!-- Main Page Content -->
@@ -36,22 +48,44 @@
                     <h1><?php echo htmlspecialchars($user["FirstName"]." ".$user["LastName"]); ?></h1>
                 </div>
                 <div class="col-4">
-                    <img src="https://proficon.stablenetwork.uk/api/identicon/<?php echo $user["UserID"];?>.svg" alt="Profile Icon" class="h-50"/>
+                    <img src="https://proficon.stablenetwork.uk/api/identicon/<?php echo $user["UserID"];?>.svg"
+                        alt="Profile Icon" class="h-50" />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <h5>Name: <?php echo htmlspecialchars($user["FirstName"]." ".$user["LastName"]);?></h5>
+                    <h5>Bastes: <?php echo htmlspecialchars($user["BasteCount"]);?></h5>
+                    <h5>Company: <?php echo htmlspecialchars($user["Company"]);?></h5>
+                    <h5>Website: <?php echo htmlspecialchars($user["Website"]);?></h5>
+                    <h5>Location: <?php echo htmlspecialchars($user["Location"]);?></h5>
+                    <h5>Github: <a
+                            href="https://github.com/<?php echo htmlspecialchars($user["Github"]);?>"><?php echo htmlspecialchars($user["Github"]);?></a>
+                    </h5>
+                    <h5>Twitter: @<a
+                            href="https://twitter.com/<?php echo htmlspecialchars($user["Twitter"]);?>"><?php echo htmlspecialchars($user["Twitter"]);?></a>
+                    </h5>
+                </div>
+            </div>
+            <div class="row mt-5">
+                <h2 class="text-center">Users Bastes</h2>
+                <div class="row">
+                    <?php while($rows = $result -> fetch_array(MYSQLI_ASSOC)) { ?>
+                        <div class="col-12 col-md-6 col-lg-4 mb-1">
+                            <div class="card">
+                                <div class="card-body">
+                                    <?php $time = strtotime($rows["CreatedAt"]); ?>
+                                    <h5 class="card-title"><?php echo htmlspecialchars($rows['BasteName']); ?></h5>
+                                    <p class="card-text"><?php echo date("d M Y @ H:i", $time) ?></p>
+                                    <a href="<?php echo $rows['BasteID']; ?>" class="btn btn-primary">View Baste</a>
+                                </div>
+                            </div>
+                        </div>
+                    <?php } ?>
                 </div>
         </div>
-        <div class="row">
-            <div class="col-12">
-                <h5>Name: <?php echo htmlspecialchars($user["FirstName"]." ".$user["LastName"]);?></h5>
-                <h5>Bastes: <?php echo htmlspecialchars($user["BasteCount"]);?></h5>
-                <h5>Company: <?php echo htmlspecialchars($user["Company"]);?></h5>
-                <h5>Website: <?php echo htmlspecialchars($user["Website"]);?></h5>
-                <h5>Location: <?php echo htmlspecialchars($user["Location"]);?></h5>
-                <h5>Github: <a href="https://github.com/<?php echo htmlspecialchars($user["Github"]);?>"><?php echo htmlspecialchars($user["Github"]);?></a></h5>
-                <h5>Twitter: @<a href="https://twitter.com/<?php echo htmlspecialchars($user["Twitter"]);?>"><?php echo htmlspecialchars($user["Twitter"]);?></a></h5>
         </div>
-    </div>
-</div>
-<!-- End Main Page Content -->
+        <!-- End Main Page Content -->
 
 
-<?php include("./res/php/footer.php"); ?>
+        <?php include("./res/php/footer.php"); ?>
