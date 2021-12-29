cde0da8ee3dd3325c4536aae63a9fb46eb812865
Skylar <Skylar.beacham@outlook.com>
2021-12-29 15:05:52 +0000

    okay, lets try this before consulting the k dog

--- res/php/addbaste.php
+++ res/php/addbaste.php
@@ -3,6 +3,7 @@
 session_start();
 include_once('_connect.php');
 include_once('functions.inc.php');
+$mysqli = $connect;
 
 
 //If user is not logged in, return to login page
@@ -13,6 +14,17 @@ if(!isset($_SESSION['UserID']))
 }
 
 
+//Get the Data needed to perform the SQL update
+$basteID = GenerateID();
+$userID = $_SESSION['UserID'];
+$basteName = $mysqli->real_escape_string($_POST['basteName']) ?? "";
+$basteContents = $mysqli->real_escape_string($_POST['basteContents']) ?? "";
+$basteVisibility = $mysqli->real_escape_string($_POST['basteVisibility']);
+$basteExpiresAt = $mysqli->real_escape_string($_POST['expiresAt']);
+$bastePasswordRequired = $mysqli->real_escape_string($_POST['passwordRequired']);
+$bastePassword = $mysqli->real_escape_string($_POST['bastePassword']) ;
+
+
 //If title, contents or visibility are not set, error out
 if(!isset($_POST['basteName']) || !isset($_POST['basteContents']) || !isset($_POST['basteVisibility']))
 {
@@ -21,37 +33,16 @@ if(!isset($_POST['basteName']) || !isset($_POST['basteContents']) || !isset($_PO
 }
 
 
-//Get the Data needed to perform the SQL update
-$basteID = GenerateID();
-$basteName = mysqli_real_escape_string($connect, $_POST['basteName']);
-$basteContents = mysqli_real_escape_string($connect, $_POST['basteContents']);
-$basteVisibility = mysqli_real_escape_string($connect, $_POST['basteVisibility']);
-$basteExpiresAt = mysqli_real_escape_string($connect, $_POST['expiresAt']);
-$bastePasswordRequired = mysqli_real_escape_string($connect, $_POST['passwordRequired']);
-$userID = $_SESSION['UserID'];
-$bastePassword = mysqli_real_escape_string($connect, $_POST['bastePassword']);
-
-
 //Generate the Template SQL Data
 $tblUsersSql = "UPDATE `tblUsers` SET `BasteCount` = `BasteCount` + 1 WHERE `UserID` = ?;";
 $tblBastesSql = "INSERT INTO `tblBastes`(`BasteID`, `BasteName`, `BasteContents`, `Visibility`, `ExpiresAt`, `PasswordRequired`, `Password`, `UserID`) VALUES (?,?,?,?,?,?,?,?);";
 
-
 $user = GetUser($connect, $userID);
 
 if($user['IsPremium'] == 1){
-    if($basteExpiresAt != ""){
-        $basteExpiresAt = date("Y-m-d H:i:s", strtotime($basteExpiresAt));
-    }
-    else{
-        $basteExpiresAt = null;
-    }
-    if($bastePasswordRequired == "true"){
-        $bastePassword = password_hash($bastePassword, 1, array('cost' => 10));
-    }
-    else{
-        $bastePassword = null;
-    }
+    $basteExpiresAt = ($basteExpiresAt == "")? NULL : date("Y-m-d H:i:s", strtotime($basteExpiresAt));
+    $bastePassword = ($bastePassword == "") ? NULL : password_hash($bastePassword, 1, array('cost' => 10));
+    $bastePasswordRequired = ($bastePassword == "") ? 0 : 1;
 }
 else{
     $basteExpiresAt = null;
@@ -67,7 +58,6 @@ if($user["CanBaste"] == 0)
 else if($user["BasteCount"] < $user["MaximumBastes"] || $user["MaximumBastes"] == null)
 {
     //Perform the SQL
-    $mysqli = $connect;
     $mysqli->autocommit(false);
     $stmt1 = $mysqli->prepare($tblBastesSql);
     $stmt1->prepare('ssssssss', $basteID, $basteName, $basteContents, $basteVisibility, $basteExpiresAt, $bastePasswordRequired, $bastePassword, $userID);
