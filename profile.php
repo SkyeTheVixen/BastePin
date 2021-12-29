14091527bcce6a7f72e032ec52ca8726203a6bbb
Skylar <Skylar.beacham@outlook.com>
2021-12-29 13:16:11 +0000

    added profile

---
+++ profile.php
@@ -0,0 +1,67 @@
+<?php session_start();?>
+<?php $title="Profile | VDBP"; ?>
+<?php $currentPage="profile"; ?>
+<?php include("res/php/_connect.php"); ?>
+<?php include("res/php/_authcheck.php"); ?>
+<?php include("res/php/header.php"); ?>
+<?php include("res/php/navbar.php"); ?>
+<?php include("res/php/functions.inc.php"); ?>
+<?php $user = getProfileById($connect, $_GET["UserID"]); ?>
+<!-- If there is an error -->
+<?php
+    if(isset($_GET["er"])) {
+        if($_GET["er"] == "insufperm") {
+            echo "<script>Swal.fire({ icon: 'warning', title: 'Oops...', text: 'You did not have sufficient permission to do that', heightAuto: false });</script>"; 
+        } else if($_GET["er"] == "nobastedel") {
+            echo "<script>Swal.fire({ icon: 'warning', title: 'Oops...', text: 'There was no baste to delete', heightAuto: false });</script>"; 
+        }
+    } 
+?>
+
+<!-- Main Page Content -->
+<div class="container">
+
+    <!-- Welcome Greeting -->
+    <div class="row">
+        <div class="col-12 mt-5 align-items-center">
+            <h1 class="text-center">Account</h1>
+        </div>
+    </div>
+    <!-- End Welcome Greeting -->
+
+    <div class="row">
+        <div class="col-12">
+            <div class="row h-25">
+                <div class="col-8">
+                    <h1><?php echo htmlspecialchars($user["FirstName"]." ".$user["LastName"]); ?></h1>
+                </div>
+                <div class="col-4">
+                    <img src="https://proficon.stablenetwork.uk/api/identicon/<?php echo $user["UserID"];?>.svg" alt="Profile Icon" class="h-50"/>
+                </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <h5>Name: <?php echo htmlspecialchars($user["FirstName"]." ".$user["LastName"]);?></h5>
+                <h5>Email: <?php echo htmlspecialchars($user["Email"]);?></h5>
+                <h5>Premium status: <?php echo htmlspecialchars($user["IsPremium"]) ? 'Premium': 'Free';?></h5>
+                <h5>Baste Count: <?php echo htmlspecialchars($user["BasteCount"]);?> / <?php echo htmlspecialchars($user["MaximumBastes"])?:'Infinity';?></h5>
+                <h5>Company: <?php echo htmlspecialchars($user["Company"]);?></h5>
+                <h5>Website: <?php echo htmlspecialchars($user["Website"]);?></h5>
+                <h5>Location: <?php echo htmlspecialchars($user["Location"]);?></h5>
+                <h5>Github: <a href="https://github.com/<?php echo htmlspecialchars($user["Github"]);?>"><?php echo htmlspecialchars($user["Github"]);?></a></h5>
+                <h5>Twitter: @<a href="https://twitter.com/<?php echo htmlspecialchars($user["Twitter"]);?>"><?php echo htmlspecialchars($user["Twitter"]);?></a></h5>
+                <div class="row mt-3">
+                    <div class="col-12">
+                        <a href="<?php echo $pageredirect; ?>editaccount" class="btn btn-primary">Edit Profile</a>
+                        <?php if($user["IsPremium"] == 0) {?>
+                            <a href="<?php echo $pageredirect; ?>premium" class="btn btn-primary">Upgrade to premium?</a>
+                        <?php } ?>
+                    </div>
+                </div>
+        </div>
+    </div>
+</div>
+<!-- End Main Page Content -->
+
+
+<?php include("./res/php/footer.php"); ?>
--- res/php/functions.inc.php
+++ res/php/functions.inc.php
@@ -55,6 +55,27 @@
         }
     }
 
+    function GetProfileByID($connect, $UserID){
+        $mysqli = $connect;
+        $UserID = $mysqli->real_escape_string($UserID);
+        $mysqli -> autocommit(false);
+        $sql = "SELECT `tblUsers`.`UserID`, `tblUsers`.`FirstName`, `tblUsers`.`LastName`, `tblUsers`.`Email`, `tblUsers`.`CanBaste`, `tblUsers`.`IsAdmin`, `tblUsers`.`IsPremium`, `tblUsers`.`IsLocked`, `tblUsers`.`BasteCount`, `tblUsers`.`MaximumBastes`, `tblProfile`.`Company`, `tblProfile`.`Location`, `tblProfile`.`Website`, `tblProfile`.`Twitter`, `tblProfile`.`Github` FROM `tblUsers` INNER JOIN `tblProfile` ON `tblUsers`.`UserID` = `tblProfile`.`UserID` WHERE `tblUsers`.`UserID` = ?";
+        $stmt  = $mysqli->prepare($sql);
+        $stmt -> bind_param('s', $UserID);
+        $stmt -> execute();
+        $result = $stmt->get_result();
+        if($result -> num_rows === 1){
+            $User = $result->fetch_array(MYSQLI_ASSOC);
+            $mysqli -> commit();
+            $stmt->close();
+            $mysqli->close();
+            return $User;
+        }
+        $mysqli -> rollback();
+        $stmt->close();
+        $mysqli->close();
+    }
+
     function sendMail($email, $userName,  $subject, $message, $altMessage){
         include_once("_pass.php");
         $mail = new PHPMailer(true);
