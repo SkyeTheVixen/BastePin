c4b05be4c0954b52d484f4046fc4b5fc4025a15a
Skylar <Skylar.beacham@outlook.com>
2022-01-01 23:12:39 +0000

    maybe thisll update

--- res/php/passreset.php
+++ res/php/passreset.php
@@ -24,7 +24,7 @@
     }
     $row = $result->fetch_array(MYSQLI_ASSOC);
     $UserID = $row["UserID"];
-    $sql = "INSERT INTO `tblPasswordResets`(`UserID`, `Token`, `Expiry`) VALUES (?,?,(now() + INTERVAL 30 MINUTE)) ON DUPLICATE KEY UPDATE";
+    $sql = "INSERT INTO `tblPasswordResets`(`UserID`, `Token`, `Expiry`) VALUES (?,?,(now() + INTERVAL 30 MINUTE)) ON DUPLICATE KEY UPDATE `Token` = ?, `Expiry` = (now() + INTERVAL 30 MINUTE)";
     $stmt = $mysqli -> prepare($sql);
     $stmt -> bind_param('ss', $UserID, $token);
     $stmt -> execute();
