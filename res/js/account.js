b864b8e7a7092f3fe896bea948b04bee3d377bc5
Skylar <Skylar.beacham@outlook.com>
2022-01-05 18:33:57 +0000

    Removed redundant JS

--- res/js/account.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-
-    if(!window.location.href.endsWith("/")){
-        console.log(window.location.href);
-        window.location.href = window.location.href + "/";
-    }
-
-});
