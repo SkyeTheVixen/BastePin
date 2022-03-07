3b0fc4af2c2129bc214050d23c24870f972a1625
Skylar <Skylar.beacham@outlook.com>
2022-03-07 23:11:55 +0000

    Temporary echo modify

--- res/php/auth.php
+++ res/php/auth.php
@@ -10,7 +10,7 @@
     $captcha = $_POST['token'];
     $secretKey = $_ENV["GRECAPTCHA_SECRET"];
     $reCAPTCHA = json_decode(file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret=' . urlencode($secretKey) .  '&response=' . urlencode($captcha)));
-    echo $reCAPTCHA->success;
+    echo $reCAPTCHA->score;
     if ($reCAPTCHA->score <= 0.5){
         echo json_encode(array("statusCode" => 207));
         return;
