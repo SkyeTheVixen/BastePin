8871a6d8bd28c85b99dd96ad70457d4aab974cc5
Skylar <Skylar.beacham@outlook.com>
2022-04-18 23:33:58 +0100

    update counter

--- baste.php
+++ baste.php
@@ -115,7 +115,7 @@
                     <div class="col-1">
                         <a id="fav" href="#" data-basteid="<?php echo $basteID;?>">
                             <h4><i id="favouriteIcon"
-                                    <?php if($favres->num_rows > 0){echo "class='fas fa-star'";} else {echo "class='far fa-star'";} ?>></i> <span class="text-muted"><?= $favcountres->FavouriteCount; ?></span>
+                                    <?php if($favres->num_rows > 0){echo "class='fas fa-star'";} else {echo "class='far fa-star'";} ?>></i> <span class="text-muted"><?= $favcountres; ?></span>
                             </h4>
                         </a>
                     </div>
